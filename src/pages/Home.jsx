199e5aa5c91c742cd0148799f6bdfb9e781a822a
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-05 19:35:21 +0530

    Added file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Bloglist from "../components/Bloglist";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Navbar />
       <Header />
       <Bloglist />
+      <Newsletter />
     </>
   );
 };
