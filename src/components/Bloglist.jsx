a5ccd58113b16c74f75aa261f27fcc84e78707c5
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-05 19:09:14 +0530

    Added File

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { blogCategories } from "../assets/assets";
+import { blog_data, blogCategories } from "../assets/assets";
 import { motion } from "motion/react";
+import BlogCard from "./BlogCard";
 
 const Bloglist = () => {
   const [menu, setmenu] = useState("All");
@@ -25,7 +26,11 @@ const Bloglist = () => {
           </div>
         ))}
       </section>
-      <section>{/* Blog cards */}</section>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data.filter((blog) => menu === "All"? true : blog.category === menu ).map((blog) => (
+              <BlogCard key={blog._id} blog={blog}/>
+        ))}
+      </section>
     </main>
   );
 };
