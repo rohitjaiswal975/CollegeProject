3b7ad6912db497ee362dc449b3bf04d0916fecc5
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-06 20:01:29 +0530

    Updated

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -27,9 +27,11 @@ const Bloglist = () => {
         ))}
       </section>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
-        {blog_data.filter((blog) => menu === "All"? true : blog.category === menu ).map((blog) => (
-              <BlogCard key={blog._id} blog={blog}/>
-        ))}
+        {blog_data
+          .filter((blog) => (menu === "All" ? true : blog.category === menu))
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
       </section>
     </main>
   );
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { assets, blog_data } from "../assets/assets";
+import Navbar from "../components/Navbar";
+import Moment from "moment";
 
 const Blog = () => {
-  return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  )
-}
+  const { id } = useParams();
 
-export default Blog
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  return data ? (
+    <div className="relative">
+      {/* Background gradient image */}
+      <img
+        src={assets.gradientBackground}
+        alt="bg-Image"
+        className="absolute -top-50 -z-10 opacity-50"
+      />
+      {/* Navbar component */}
+      <Navbar />
+
+      {/* Blog Header */}
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-primary py-4 font-medium">
+          Published on {Moment(data.createdAt).format("MMM Do YYYY")}
+        </p>
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">
+          {data.title}
+        </h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
+          Michael Brown
+        </p>
+      </div>
+
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+
+        <div className="rich-text max-w-3xl mx-auto" dangerouslySetInnerHTML={{ __html: data.description }}></div>
+      </div>
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
+};
+
+export default Blog;
