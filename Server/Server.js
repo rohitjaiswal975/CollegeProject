76e339a568831ecaa078142f14e52c2ebfb14e05
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-13 18:39:06 +0530

    Added Files

--- Server/Server.js
+++ Server/Server.js
@@ -2,16 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDb from "./Configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 const app = express();
 
 // Connected Mongodb
-
 await connectDb();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.send("Api is working");
