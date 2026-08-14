607790ad83d75ea7bc4a16135e2447d29c0ac018
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-14 19:47:51 +0530

    Added File

--- Server/routes/blogRoute.js
+++ Server/routes/blogRoute.js
@@ -1,15 +1,27 @@
 import express from "express";
-import { addBlog, deleteBlogByID, getAllBlogs, getBlogId, togglePublish } from "../controllers/BlogController.js";
+import {
+  addBlog,
+  addComment,
+  deleteBlogByID,
+  getAllBlogs,
+  getBlogComment,
+  getBlogId,
+  togglePublish,
+} from "../controllers/BlogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/Auth.js";
 
 const blogRoute = express.Router();
 
-blogRoute.post("/add", upload.single("image"), auth , addBlog);
+blogRoute.post("/add", upload.single("image"), auth, addBlog);
 
-blogRoute.get("/all",getAllBlogs)
-blogRoute.get("/:blogid",getBlogId)
-blogRoute.post("/delete",auth , deleteBlogByID)
-blogRoute.post("/toggle-publish",auth , togglePublish)
+blogRoute.get("/all", getAllBlogs);
+blogRoute.get("/:blogid", getBlogId);
+blogRoute.post("/delete", auth, deleteBlogByID);
+blogRoute.post("/toggle-publish", auth, togglePublish);
+
+// Comment route
+blogRoute.post("/add-comment", addComment);
+blogRoute.get("/comments", getBlogComment);
 
 export default blogRoute;
