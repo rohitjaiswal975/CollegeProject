b9618c1f109d4d6896e99c0a468d2d4cf6efe28f
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-16 19:03:09 +0530

    Edited

--- Server/controllers/BlogController.js
+++ Server/controllers/BlogController.js
@@ -61,8 +61,8 @@ export const addBlog = async (req, res) => {
 
 export const getAllBlogs = async (req, res) => {
   try {
-    const blog = await Blog.find({ isPublished: true });
-    res.json({ success: true, blog });
+    const blogs = await Blog.find({ isPublished: true });
+    res.json({ success: true, blogs });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
