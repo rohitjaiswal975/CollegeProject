1b999e9810d883b5dde78dc57a061eafad03b0a6
Rohit Jaiswal <rj2932007@gmail.com>
2026-08-16 12:34:47 +0530

    Bug Fix

--- Server/controllers/adminController.js
+++ Server/controllers/adminController.js
@@ -12,7 +12,7 @@ export const adminLogin = async (req, res) => {
       return res.json({ success: false, message: "Invalid Credentials" });
     }
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
-    res.json({ success: true, Token: token });
+    res.json({ success: true,token });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
